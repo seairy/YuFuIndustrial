268b939f059f727a0682f82f5a4d2ba97ccb5100
seairy <seairy@gmail.com>
2015-05-20 19:10:20 +0800

    init commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:seairy/YuFuIndustrial.git'
 set :keep_releases, 3
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/uploads}
 
 set :use_sudo, false
 
